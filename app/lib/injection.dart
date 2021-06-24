745a06f24c34da633a8e0c119d60c31333e26b35
wzslr321 <wiktor.zajac888@gmail.com>
2021-06-24 16:42:22 +0200

    injectable configuration

---
+++ app/lib/injection.dart
@@ -0,0 +1,19 @@
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
+
+import 'injection.config.dart';
+
+/// [GetIt] instance required for [injectable] configuration.
+GetIt s1 = GetIt.instance;
+
+/// Default [injectable] configuration, marked with
+/// [injectableInit] for code generation.
+@injectableInit
+void configureInjection(String env) {
+  $initGetIt(s1, environment: env);
+}
+
+Future<void> init() async {
+  s1.registerLazySingleton(() => InternetConnectionChecker());
+}
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
 
 import 'presentation/core/my_app_widget.dart';
+import 'injection.dart' as d1;
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  d1.configureInjection(Environment.dev);
+  await d1.init();
   runApp(MyAppWidget());
 }
