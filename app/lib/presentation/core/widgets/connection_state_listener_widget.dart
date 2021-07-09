a1be87cd612ed6ac7efdb77f3a343c7c6c965f5d
Yunus AK <yunus.ak.gen.tr@gmail.com>
2021-07-09 13:37:15 +0300

    add ConnectionStateListenerWidget

---
+++ app/lib/presentation/core/widgets/connection_state_listener_widget.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart' hide ConnectionState;
+// ConnectionState hided because ConnectionBloc already has a class with the same name
+// renaming the class or using a namespace for connection_bloc are also possible options
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../application/connection/connection_bloc.dart';
+import 'simple_snackbar.dart';
+
+/// A top level widget that listens connection state
+/// Must be located under the MetarialApp context to be able to show snackbar messages
+class ConnectionStateListenerWidget extends StatelessWidget {
+  /// ConnectionStateListenerWidget
+  const ConnectionStateListenerWidget({required this.child});
+
+  /// child widget
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<ConnectionBloc, ConnectionState>(
+      listener: (context, state) {
+        state.map(
+          connected: (_) => ScaffoldMessenger.of(context)
+              .showSnackBar(SimpleSnackBar('Connected!')),
+          connectedOnInitial: (_) {},
+          connectedWithNoNetworkAccess: (_) {},
+          disconnected: (_) => ScaffoldMessenger.of(context)
+              .showSnackBar(SimpleSnackBar('Disconnected!')),
+          initial: (_) {},
+        );
+      },
+      child: child,
+    );
+  }
+}
