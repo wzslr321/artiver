652f943268475d141d350f931d588acb9b6f9949
wzslr321 <wiktor.zajac888@gmail.com>
2021-07-13 03:16:30 +0200

    add makefile conf to handle multiple main.go files

--- server/api/main.go
+++ server/api/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"time"
+)
+
+func main() {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+	err = client.Ping(ctx, readpref.Primary())
+}
